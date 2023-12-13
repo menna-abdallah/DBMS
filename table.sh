26b24a223a95d87ee0c9d8ebdcaf3eb6a8b804dc
Menna Abdallah <menna.abdallh11@gmail.com>
2023-12-13 22:12:43 +0200

    modify

--- table.sh
+++ table.sh
@@ -1,5 +1,22 @@
 #! /bin/bash
 
+read -p " Enter DataBase Name: " dbname
+
+if [ ! -d MyDBMS/$dbname ];
+then
+	read -p "$dbname not exsit , Do you want to creat one? (y / n): " opt
+	 if [ opt = 'y' -o opt = 'Y' ];
+	 then
+		 source ../creat_DB.sh
+		
+		 cd MyDBMS/$dbname
+	 else
+		source ../DB.sh
+	 fi
+ else
+	 cd MyDBMS/$dbname
+fi
+
 select option in "create table" "select" "insert" "update" "remove" "truncate"
 do
 	case $option in 
