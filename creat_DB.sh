1235c263c090c6e87059c7e0dc3dc05943906c8c
Menna Abdallah <menna.abdallh11@gmail.com>
2023-12-13 06:33:29 +0200

    add create data base (dir) script

---
+++ creat_DB.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+shopt -s extglob
+LC_COLLATE=C
+
+#TILL VALID NAME
+while true
+do
+
+read -p "Enter DataBase Name" DBNAME
+
+#CHECK EXITANCE
+if [ -e "$DBNAME" ];
+then
+	echo "$DBNAME is alredy exist"
+
+	# in database naming : the name of  
+# ... ${#DBNAME} = The length of BDNAME
+elif [[ ${#DBNAME} -lt 1 || ${#DBNAME} -gt 64 ]];
+then
+      	echo " DataBase names must be in range of [1-64]"
+elif [[ $DBNAME =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]] ;
+then
+       	if [[ $DBNAME =~ [[:space:]] ]]
+ then
+	 echo " DataBase Name is unvalid with spaces"
+	 DBNAME=${DBNAME// /_}
+	 echo "your DataBase name is $DBNAME"
+
+ else
+	 echo "Valid Name"
+	 mkdir $(pwd)/$DBNAME
+	 echo " $DBNAME DataBase created successfully"
+	 break
+fi
+else
+	echo "Invalid Name"
+fi
+
+done
