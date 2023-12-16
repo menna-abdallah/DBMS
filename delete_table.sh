463469964a0a9aee62344eb643d199269d17ff04
Menna Abdallah <menna.abdallh11@gmail.com>
2023-12-16 02:01:00 +0200

    edit

--- delete_table.sh
+++ delete_table.sh
@@ -5,17 +5,21 @@ LC_COLLATE=C
 
 select del_choice in "Delete all Data" "Delete Rows" "Delete Colums" "EXIT"
 do
-	case del_choice in
+	case $del_choice in
 		"Delete all Data")
+			t=$(awk 'NR==1 {print $0}' testtable)
+			echo "$t"
+			echo "$t" > newtable
+			cat newtable
 
 			;;
 		"Delete Rows")
-
+echo "mmm"
 			;;
 		"Delete Colums")
-
+echo"mnnnn"
 			;;
-		"Exit")
+		"EXIT")
 			exit
 			;;
 		*)
